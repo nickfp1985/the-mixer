814a7924b7adc66e8ddd418bceeca1494625c1c4
philtd5000 <philiptaft9@gmail.com>
2018-06-30 13:18:38 -0700

    working product!

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -22,14 +22,22 @@ $(document).ready(function () {
                 let div = $('<div>');
                 let imgSrc = drinkName.strDrinkThumb;
 
-                let img = $(`<img src="${imgSrc}" id="${drinkName.idDrink}">`);
-                img.addClass('drinks').css({ "height": "250px", "width": "250px", "border-radius": "10px", "margin": "10px" });
+                let img = $(`<img src="${imgSrc}" id="${drinkName.idDrink}" class="results-img">`);
+                img.addClass('drinks')
+                    .css({ "height": "250px", "width": "250px", "border-radius": "10px", "margin": "10px" })
+                    .on('click', function () {
+                        $('.landing-page').hide();
+                        $('.container-results').empty();
+                        $('.container-results').show();
+                    });
                 let name = $('<p>');
+
+
                 name.text(drinkName.strDrink);
                 name.addClass('name').css({ "text-align": "center" });
 
                 //COPY AND PASTE INDIVIDUAL .AJAX REQUEST CODE HERE:
-               
+
                 //  END INDIVIDUAL .AJAX REQUESET CODE    */
 
                 // append info to page 
@@ -51,12 +59,121 @@ $(document).ready(function () {
 
     /*END NIGEL CODE:*/
 
+    $(document).on('click', '.results-img', function () {
+
+        console.log($(this).attr('id'));
+
+        $('.container-results').hide();
+        $('.final-drink').show();
+        /* https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13060 : EXAMPLE URL FOR ID SEARCH */
+
+        let findByIdBaseURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+        let drinkID = $(this).attr('id');
+        let fullQueryURLByID = findByIdBaseURL + drinkID;
+        fullQueryURLByID.toString().trim();
+
+        $.ajax({
+            url: fullQueryURLByID,
+            method: "GET"
+        }).then(function (res) {
+
+            //HERE ARE THE RESULTS WE WILL USE TO GENERATE NEW PAGE WITH COMPLETE DRINK INFORMATION INCLUDING INREDIENTS, AMOUNTS, HOW TO INSTRUCTIONS ETC.
+            let data = res.drinks[0];
+            console.log(data);
+            console.log(Object.keys(data));
+
+            let drinkObj = {}
+
+            drinkObj.imgSrc = data.strDrinkThumb;
+            drinkObj.name = data.strDrink;
+            drinkObj.withAlcohol = data.strAlcoholic;
+            drinkObj.glassType = data.strGlass;
+            drinkObj.instructions = data.strInstructions;
+            drinkObj.ingredients = [];
+            drinkObj.amounts = [];
+
+            //PULLING INGREDIENTS INTO ARRAY:
+            //CREATE AN ARRAY FROM OBJECT ENTRIES
+            let drinkInfoArray = Object.entries(data);
+            //CREATE AN ARRAY OF ONLY INGREDIENT PAIRS:
+            let drinkIngredients = [];
+            for (let i = 9; i < 24; i++) {
+                drinkIngredients.push(drinkInfoArray[i]);
+            }
+            //CREATE FINAL ARRAY OF INGREDIENTS:
+            for (let i = 0; i < drinkIngredients.length; i++) {
+                let individualIngredient = drinkIngredients[i][1];
+                if (individualIngredient === "") {
+                } else if (individualIngredient) {
+                    drinkObj.ingredients.push(individualIngredient);
+                }
+            }
+            console.log("INGREDIENTS");
+            console.log(drinkObj.ingredients);
+
+            let amountsInfoArray = Object.entries(data);
+            let drinkAmounts = [];
+            for (let i = 24; i < 39; i++) {
+                drinkAmounts.push(amountsInfoArray[i]);
+            }
+            //CREATE FINAL ARRAY OF AMOUNTS:
+            for (let i = 0; i < drinkAmounts.length; i++) {
+                let individualAmount = drinkAmounts[i][1];
+                if (individualAmount === " ") {
+                } else if (individualAmount) {
+                    drinkObj.amounts.push(individualAmount);
+                }
+            }
+            console.log("AMOUNTS:");
+            console.log(drinkObj.amounts);
+
+            $('.drink-name').text(`${drinkObj.name}`);
+            $('.drink-alcoholic').text(`${drinkObj.withAlcohol}`);
+            $('.drinkThumb2').attr('src', `${drinkObj.imgSrc}`);
+            $('.drink-glass').text(`${drinkObj.glassType}`);
+            $('.instructions').html(`<p>${drinkObj.instructions}</p>`);
+            $('.list').empty();
+
+            for (let i = 0; i < drinkObj.ingredients.length; i++) {
+
+                let $row = $('<tr>').addClass(`item:${i}`);
+
+                let $ingredient = $(`<td class="ingredient border-right">${drinkObj.ingredients[i]}</td>`);
+                let $amount = $(`<td class="measure">${drinkObj.amounts[i]}</td>`);
+
+                $row.append($ingredient);
+                $row.append($amount);
+
+                $('.list').append($row);
+
+            }
+
+
+        });
+
+
+    })
+
+    $('#newSearch').on('click', function (event) {
+
+        event.preventDefault();
+
+        $('.landing-page').show();
+        $('.container-results').empty();
+        $('.random-four').empty();
+        $('.final-drink').hide();
+        randomDrink();
+
+    })
+
     $('#searchName').on('click', function (event) {
 
         event.preventDefault();
 
-        //CLEAR PREVIOUS RESULTS ON INDEX.HTML:
+        $('.landing-page').hide();
         $('.container-results').empty();
+        $('.container-results').show();
+
 
         //DEVELOPING QUERY URL STRING FROM DRINK-NAME INPUT:
         let $drinkName = $('#inputDrinkName').val().trim();
@@ -82,105 +199,21 @@ $(document).ready(function () {
             for (let i = 0; i < resultsLength; i++) {
                 let $target = $('.container-results');
 
-                let $img = $(`<img src="${res.drinks[i].strDrinkThumb}" id="${res.drinks[i].idDrink}" >`)
-                            .addClass('img-fluid')
-                            .addClass('drinkThumb1');
+                let $img = $(`<img src="${res.drinks[i].strDrinkThumb}" id="${res.drinks[i].idDrink}" class="results-img">`)
+                    .addClass('img-fluid')
+                    .addClass('drinkThumb1');
 
                 //APPLYING CLICK LISTENER TO EACH 'THUMBNAIL RESULT' - ON CLICK THE DIV WILL NEED TO:
                 //PASSING .AJAX CALL TO EACH RESULTS IMAGE ON CLICK:
-                $img.on('click', function () {
+                // $img.on('click', function () {
 
-                    console.log($(this).attr('id'));
-                    /* https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13060 : EXAMPLE URL FOR ID SEARCH */
+                //     //IF ERROR RETURN PASTE HERE:
 
-                    let findByIdBaseURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-                    let drinkID = $(this).attr('id');
-                    let fullQueryURLByID = findByIdBaseURL + drinkID;
-                    fullQueryURLByID.toString().trim();
+                // }); /*end image click listener*/
 
-                    $.ajax({
-                        url: fullQueryURLByID,
-                        method: "GET"
-                    }).then(function (res) {
-
-                        //HERE ARE THE RESULTS WE WILL USE TO GENERATE NEW PAGE WITH COMPLETE DRINK INFORMATION INCLUDING INREDIENTS, AMOUNTS, HOW TO INSTRUCTIONS ETC.
-                        let data = res.drinks[0];
-                        console.log(data);
-                        console.log(Object.keys(data));
-                        
-                        let drinkObj = {}
-
-                        drinkObj.imgSrc = data.strDrinkThumb;
-                        drinkObj.name = data.strDrink;
-                        drinkObj.withAlcohol = data.strAlcoholic;
-                        drinkObj.glassType = data.strGlass;
-                        drinkObj.ingredients = [];
-                        drinkObj.amounts = [];
-                        
-                        //PULLING INGREDIENTS INTO ARRAY:
-                        //CREATE AN ARRAY FROM OBJECT ENTRIES
-                        let drinkInfoArray = Object.entries(data);
-                        //CREATE AN ARRAY OF ONLY INGREDIENT PAIRS:
-                        let drinkIngredients = [];
-                        for (let i = 9; i < 24; i++) {
-                            drinkIngredients.push(drinkInfoArray[i]);
-                        }
-                        //CREATE FINAL ARRAY OF INGREDIENTS:
-                        for (let i = 0; i < drinkIngredients.length; i++) {
-                            let individualIngredient = drinkIngredients[i][1];    
-                            if (individualIngredient === "") {
-                            } else if (individualIngredient) {
-                                drinkObj.ingredients.push(individualIngredient);
-                            }
-                        }
-                        console.log("INGREDIENTS");
-                        console.log(drinkObj.ingredients);
-
-                        let amountsInfoArray = Object.entries(data);
-                        let drinkAmounts = [];
-                        for (let i = 24; i < 39; i++){
-                            drinkAmounts.push(amountsInfoArray[i]);
-                        }
-                        //CREATE FINAL ARRAY OF AMOUNTS:
-                        for (let i = 0; i < drinkAmounts.length; i++){
-                            let individualAmount = drinkAmounts[i][1];
-                            if (individualAmount === " ") {
-                            } else if (individualAmount) {
-                                drinkObj.amounts.push(individualAmount);
-                            }
-                        }
-                        console.log("AMOUNTS:");
-                        console.log(drinkObj.amounts);
-
-                        console.log(drinkObj);
-
-                        $('.drink-name').text(`${drinkObj.name}`);
-                        $('.drink-alcoholic').text(`${drinkObj.withAlcohol}`);
-                        $('.drinkThumb2').attr('src', `${drinkObj.imgSrc}`);
-                        $('.drink-glass').text(`${drinkObj.glassType}`);
-
-                        for(let i = 0; i < drinkObj.ingredients.length; i++) {
-
-                            let $row = $('<tr>').addClass(`item:${i}`);
-    
-                            let $ingredient = $(`<td class="ingredient border-right">${drinkObj.ingredients[i]}</td>`);
-                            let $amount = $(`<td class="measure">${drinkObj.amounts[i]}</td>`);
-    
-                            $row.append($ingredient);
-                            $row.append($amount);
-    
-                            $('.list').append($row);
-
-                        }
-                        
-
-                    });
-
-                }); /*end image click listener*/
-                            
                 let $name = $(`<h4>${res.drinks[i].strDrink}</h4>`)
-                            .css({ "text-align": "center" });
-                
+                    .css({ "text-align": "center" });
+
                 $img.append($name);
                 $target.append($img);
 
