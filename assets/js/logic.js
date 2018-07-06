2c0820dd33c39d1732188152ced0a41b60864c4d
Nick Peddie <nickfp1985@users.noreply.github.com>
2018-07-05 19:00:27 -0700

    update logic.js
    - added logic for the firebase login and account authentication
    - added show/hide js for landing-page, based on successful or unsuccessful login

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,61 @@
 $(document).ready(function () {
+    const database = firebase.database();
+    const ref = database.ref();
 
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // USER IS SIGNED IN -> hide the login div and show logout
+            document.getElementById("login-id").style.display = "none";
+            document.getElementById("logout-id").style.display = "block";
+
+            let user = firebase.auth().currentUser;
+            if (user != null) {
+                let userEmail = user.email;
+                $('#login-msg').text("You are logged in as: " + userEmail);
+                document.getElementsByClassName("landing-page")[0].style.display = "initial";
+            }
+        } else {
+            // NO USER SIGNED IN -> hide the logout div and show login
+            document.getElementById("login-id").style.display = "block";
+            document.getElementById("logout-id").style.display = "none";
+        }
+    });
+
+    // BUTTON CLICK -> to authenticate user input against firebase db
+    $('.login-btn').on('click', function (event) {
+        event.preventDefault();
+
+        // get user input email and password
+        let userEmail = $('#user-email').val().trim();
+        let userPw = $('#user-pw').val().trim();
+        console.log("user has logged into account!");
+        console.log(userEmail + " " + userPw);
+
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPw).catch(function (error) {
+            // handle login errors here
+            let errorCode = error.code;
+            let errorMessage = error.message;
+
+            console.log("Error : " + errorMessage);
+            document.getElementsByClassName("alert")[0].style.display = "initial";
+        });
+
+    })
+
+    $('.logout-btn').on('click', function (event) {
+        event.preventDefault();
+
+        firebase.auth().signOut().then(function() {
+        // logout successful
+        document.getElementsByClassName("landing-page")[0].style.display = "none";
+        $('.container-results').empty();
+        $('.final-drink').empty();
+        }).catch(function(error) {
+        // an error happened during logout
+        });
+
+    })
+    //*******************//
     //RENDER FOUR RANDOM DRINKS ON PAGE LOAD:
     randomDrink(4);
 
